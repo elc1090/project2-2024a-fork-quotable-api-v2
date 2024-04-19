d5b889b012a0c68609b2bfa49b271e908cfaf52f
Luiz Felipe Cavalheiro <lfsantos@inf.ufsm.br>
2024-04-19 17:14:19 -0300

    conexao com LibreTranslate API

--- quotable-api/src/services/quoteAPI.js
+++ quotable-api/src/services/quoteAPI.js
@@ -1,10 +1,15 @@
 //import { fetchQuotesByAuthor } from '@/services/apiService';
+import translate from "translate";
 import { fetchRandomQuotes, fetchRandomListQuotes, fetchQuotesByAuthor, fetchSearchQuotes, fetchSearchAuthors } from './apiService.js';
+
+translate.engine = "google"; // "google", "yandex", "libre", "deepl"
+translate.key = process.env.DEEPL_KEY;
 
 async function exemploFetchQuotesByAuthor() {
   try {
     const resultado = await fetchQuotesByAuthor({ slug: 'albert-einstein' });
-    console.log(resultado);
+    const text = await translate(resultado.results[0].bio, "pt");
+    console.log(text);
   } catch (error) {
     console.error('Erro ao buscar citações por autor:', error);
   }
